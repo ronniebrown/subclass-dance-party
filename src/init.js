f7def21f48ee10c9e3c0629588692a8edfb606fa
Ronnie Brown <ronnievoice@gmail.com>
2015-05-20 00:04:37 -0700

    finishing touches

--- src/init.js
+++ src/init.js
@@ -42,8 +42,13 @@ $(document).ready(function(){
 
 
   $(".jungleFloor").on("click", function(){
-    $("body").css("background-image", "url(http://cdn2.stocksy.com/a/Wy3000/z0/15284.jpg)");
+    $("body").css("background-image", "url(https://snap-photos.s3.amazonaws.com/img-thumbs/960w/UI30ICNGU6.jpg)");
     $("body").append("<div class='jungleFloor'><h1>Break it dowwwwwnnnn</h1></div>");
+  });
+
+  $(".makeGif").on("click", function(){
+    // $("body").append("");
+    $("body").append("<div><iframe src='http://giphy.com/embed/DypHcxyEqRzHi' width='120' height='120' margin-left='300' frameBorder='0' style='max-width: 100%' class='giphy-embed' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>");
   });
 
   $(".boogie").on("click", function () {
